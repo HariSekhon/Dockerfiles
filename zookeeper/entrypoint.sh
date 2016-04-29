5a09ce4ea2237f2da78443aabfba14ae183f1084
Hari Sekhon <harisekhon@gmail.com>
2016-04-29 16:48:15 +0100

    added entrypoint.sh

---
+++ zookeeper/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-04-29 16:46:30 +0100 (Fri, 29 Apr 2016)
+#
+#  https://github.com/harisekhon/Dockerfiles
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+/zookeeper/bin/zkServer.sh start
+sleep 2
+/zookeeper/bin/zkCli.sh

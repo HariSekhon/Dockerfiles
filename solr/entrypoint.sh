3686f751ff9e6c0e91255becb2170de97c2f6aee
Hari Sekhon <harisekhon@gmail.com>
2016-05-24 17:01:46 +0100

    updated entrypoint.sh

--- solr/entrypoint.sh
+++ solr/entrypoint.sh
@@ -28,6 +28,13 @@ cd "$SOLR_HOME"
 if [ $# -gt 0 ]; then
     exec $@
 else
-    cd "$SOLR_HOME/example"
-    java -jar start.jar
+    # Solr 5+ insists on SOLR_HOME being set to /solr/server/solr dir containing solr.xml
+    set +o pipefail # in case solr version doesn't exist in older versions
+    if [ "$(solr version|cut -c 1)" -ge 5 ]; then
+        export SOLR_HOME="$SOLR_HOME/server/solr"
+        solr start -f
+    else
+        cd "$SOLR_HOME/example"
+        java -jar start.jar
+    fi
 fi

01292456d398e6711b9f8eca492aca90bd2150ab
Hari Sekhon <harisekhon@gmail.com>
2016-07-20 11:19:16 +0100

    updated entrypoint.sh

--- hbase/entrypoint.sh
+++ hbase/entrypoint.sh
@@ -26,15 +26,17 @@ mkdir /hbase/logs
 # tries to run zookeepers.sh distributed via SSH, run zookeeper manually instead now
 #RUN sed -i 's/# export HBASE_MANAGES_ZK=true/export HBASE_MANAGES_ZK=true/' /hbase/conf/hbase-env.sh
 /hbase/bin/hbase zookeeper &>/hbase/logs/zookeeper.log &
-/hbase/bin/start-hbase.sh | grep -v "ssh: command not found"
+/hbase/bin/start-hbase.sh
 /hbase/bin/hbase-daemon.sh start rest
 /hbase/bin/hbase-daemon.sh start thrift
 #/hbase/bin/hbase-daemon.sh start thrift2
 /hbase/bin/hbase shell
-echo -e "\n\nShutdown avoided by default EOF to avoid confusion for people not running this image in interactive mode, see https://github.com/harisekhon/Dockerfiles/issues/2"
+echo -e "\n\nAutomatic shutdown upon leaving HBase shell is now avoided by default to avoid confusion for people not running this image in non-interactive mode (which immediately closes the shell via the implicit EOF)
+
+for details see https://github.com/harisekhon/Dockerfiles/issues/2\n\n"
 read -p "Press 'Y' and enter if you want to shut down HBase, otherwise will leave it running: " answer
 if [ "$answer" = "Y" ]; then
-    /hbase/bin/stop-hbase.sh | grep -v "ssh: command not found"
+    /hbase/bin/stop-hbase.sh
     pkill -f org.apache.hadoop.hbase.zookeeper
     sleep 1
 else

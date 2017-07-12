7cd4b81b4cc601059f2e1a2af4f86439305347fc
Hari Sekhon <harisekhon@gmail.com>
2017-07-12 12:41:43 +0100

    updated solr-start.sh

--- solr/solr-start.sh
+++ solr/solr-start.sh
@@ -24,16 +24,12 @@ export SOLR_HOME="/solr"
 
 cd "$SOLR_HOME"
 
-if [ $# -gt 0 ]; then
-    exec $@
+# Solr 5+ insists on SOLR_HOME being set to /solr/server/solr dir containing solr.xml
+set +o pipefail # in case solr version doesn't exist in older versions
+if [ "$(solr version|cut -c 1)" -ge 5 ]; then
+    export SOLR_HOME="$SOLR_HOME/server/solr"
+    solr start -f
 else
-    # Solr 5+ insists on SOLR_HOME being set to /solr/server/solr dir containing solr.xml
-    set +o pipefail # in case solr version doesn't exist in older versions
-    if [ "$(solr version|cut -c 1)" -ge 5 ]; then
-        export SOLR_HOME="$SOLR_HOME/server/solr"
-        solr start -f
-    else
-        cd "$SOLR_HOME/example"
-        java -jar start.jar
-    fi
+    cd "$SOLR_HOME/example"
+    java -jar start.jar
 fi

80bb6bcb4ad68003aecf6aad19695eb5d12961f0
Hari Sekhon <harisekhon@gmail.com>
2017-07-12 12:36:44 +0100

    added solr user support

--- solrcloud/entrypoint.sh
+++ solrcloud/entrypoint.sh
@@ -18,40 +18,18 @@ set -euo pipefail
 
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export JAVA_HOME="${JAVA_HOME:-/usr}"
-
-export SOLR_HOME="/solr"
-
 export SOLR_USER="solr"
-WHOAMI="$(whoami)"
-if [ -n "$WHOAMI" -a "$WHOAMI" != "root" ]; then
-    export SOLR_USER="$WHOAMI"
-fi
-
-set +o pipefail
 
 if [ $# -gt 0 ]; then
     exec $@
 else
-    su - "$SOLR_USER" <<-EOF
-
-    # preserve path from root Dockerfile which has location of $SOLR_HOME/bin
-    export PATH=$PATH
-
-    # solr -e cloud fails if not called from $SOLR_HOME
-    cd "$SOLR_HOME"
-
-    if [ $# -gt 0 ]; then
-        exec $@
-    else
-        # exits with 141 for pipefail breaking yes stdout
-        set +o pipefail
-        solr -e cloud -noprompt
-        if ls -d "$SOLR_HOME"-4* &>/dev/null; then
-            tail -f "$SOLR_HOME/"node*/logs/*
-        else
-            tail -f "$SOLR_HOME/example/cloud/"node*/logs/*
-        fi
-    fi
+    if [ "$(whoami)" = "root" ]; then
+        su - "$SOLR_USER" <<-EOF
+            # preserve PATH from root
+            export PATH="$PATH"
+            /solr-start.sh
 EOF
+    else
+        /solr-start.sh
+    fi
 fi

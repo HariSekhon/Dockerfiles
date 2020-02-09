12ffb94f3d7390f5365e56cbea3e327c91abca11
Hari Sekhon <harisekhon@gmail.com>
2020-02-09 17:02:42 +0000

    updated check_repo_names.sh

--- tests/check_repo_names.sh
+++ tests/check_repo_names.sh
@@ -24,6 +24,7 @@ for x in *; do
     [ -d "$x" ] || continue
     [ "$x" = "bash-tools" ] && continue
     [ "$x" = "pytools_checks" ] && continue
+    [[ "$x" =~ devops-bash-tools* ]] && continue
     [[ "$x" =~ devops-perl-tools* ]] && continue
     [[ "$x" =~ devops-python-tools* ]] && continue
     [ -f "$x/Makefile" ] || continue

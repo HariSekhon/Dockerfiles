0648fc953b4f09fa8c71e2710e3aaa57a378216a
Hari Sekhon <harisekhon@gmail.com>
2020-04-28 16:48:53 +0100

    added build2.sh

---
+++ nagios-plugins-cassandra/build2.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2019-02-21 14:38:20 +0000 (Thu, 21 Feb 2019)
+#
+#  https://github.com/harisekhon/Dockerfiles
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euxo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+nagios_plugins="/github/nagios-plugins"
+
+bash_tools="$nagios_plugins/bash-tools"
+
+#cd "$nagios_plugins"
+
+#git pull
+#make update2
+
+#cd bash-tools
+#git pull origin master
+
+cd /
+
+"$bash_tools/setup/download_cassandra.sh"
+
+apt-get autoremove -y
+
+apt-get clean
+
+"$bash_tools/check_docker_clean.sh"

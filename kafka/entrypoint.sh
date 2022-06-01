ab105709c42015e86c7ad34f3da650f45b8ed9cc
Hari Sekhon <harisekhon@gmail.com>
2022-06-01 18:09:41 +0100

    updated entrypoint.sh

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -19,10 +19,15 @@ set -euo pipefail
 ADVERTISED_HOSTNAME="${ADVERTISED_HOSTNAME:-127.0.0.1}"
 
 zookeeper-server-start.sh /kafka/config/zookeeper.properties &
+
 echo "*** waiting for 10 secs to give ZooKeeper time to start up"
+
 sleep 10
+
 echo "# ============================================================================ #"
 echo "                         S t a r t i n g   K a f k a"
 echo "# ============================================================================ #"
-perl -pi.orig -e "s/\s*#?\s*advertised.host.name.*/advertised.host.name=${ADVERTISED_HOSTNAME}/" "/kafka/config/server.properties" && \
+
+perl -pi.orig -e "s/\\s*#?\\s*advertised.host.name.*/advertised.host.name=${ADVERTISED_HOSTNAME}/" "/kafka/config/server.properties"
+
 kafka-server-start.sh /kafka/config/server.properties
